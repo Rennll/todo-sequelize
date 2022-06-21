3972a18c267c27ea37e5b73e967d1fe9bbe5ffd2
Rennll <dt616717@gmail.com>
2022-06-22 00:44:56 +0800

    feat: add index page

--- app.js
+++ app.js
@@ -14,6 +14,15 @@ app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
+app.get('/', (req, res) => {
+  return Todo.findAll({
+    raw: true,
+    nest: true
+  })
+    .then((todos) => { return res.render('index', { todos: todos }) })
+    .catch((error) => { return res.status(422).json(error) })
+})
+
 app.get('/users/login', (req, res) => {
   res.render('login')
 })
