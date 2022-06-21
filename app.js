fa9028cffd2eee33536b6c731406dc95944115d3
Rennll <dt616717@gmail.com>
2022-06-21 23:49:06 +0800

    feat: associate Todo and User models

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const bcrypt = require('bcryptjs')
+
+const db = require('./models')
+const User = db.User
+const Todo = db.Todo
+
 const app = express()
 const PORT = 3000
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
@@ -29,7 +34,10 @@ app.get('/users/register', (req, res) => {
 })
 
 app.post('/users/register', (req, res) => {
-  res.send('register')
+  const { name, email, password, confirmPassword } = req.body
+
+  User.create({ name, email, password })
+    .then(user => res.redirect('/'))
 })
 
 app.get('/users/logout', (req, res) => {
