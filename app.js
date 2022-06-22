c3aba1b2554720af82a00f81d028e2071d1438b3
Rennll <dt616717@gmail.com>
2022-06-22 15:21:23 +0800

    feat: add passport auth login

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const bcrypt = require('bcryptjs')
+const session = require('express-session')
+const passport = require('passport')
 
+const UsePassport = require('./config/passport')
 const db = require('./models')
 const User = db.User
 const Todo = db.Todo
@@ -13,6 +16,12 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
+UsePassport(app)
 
 app.get('/', (req, res) => {
   return Todo.findAll({
@@ -34,9 +43,10 @@ app.get('/users/login', (req, res) => {
   res.render('login')
 })
 
-app.post('/users/login', (req, res) => {
-  res.send('login')
-})
+app.post('/users/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/users/login'
+}))
 
 app.get('/users/register', (req, res) => {
   res.render('register')
